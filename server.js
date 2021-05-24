2f5bc4cc492be353c5693566bdd32c3370f521ee
Bulubuch <bulubuch@gmail.com>
2021-05-24 17:17:28 +0200

    Update device-dao

--- server.js
+++ server.js
@@ -28,17 +28,17 @@ const utils = require('./utils/utils');
 const appSettings = require('./config/app-settings');
 // Routing
 const routes = require('./controllers/routes');
+const telemetry = require('./controllers/telemetry');
 
 var mqtt    = require('mqtt');
-var client  = mqtt.connect("mqtt://localhost",{clientId:"modulabsrv01"});
-console.log("connected flag  " + client.connected);
+var client  = mqtt.connect("mqtt://localhost",{clientId:"modulabSubscriber"});
 
 //handle incoming messages
 client.on('message',function(topic, message, packet){
 	console.log("message is "+ message);
 	console.log("topic is "+ topic);
 	if (topic == "telemetry") {
-		
+		telemetry.process(message);
 	}
 });
 
@@ -50,38 +50,9 @@ client.on("connect",function(){
 client.on("error",function(error){
 	console.log("Can't connect" + error);
 });
-//publish
-function publish(topic,msg,options){
-	console.log("publishing",msg);
 
-	if (client.connected == true){
-		client.publish(topic,msg,options);
-	}
-	count+=1;
-	if (count==2){ //ens script
-		clearTimeout(timer_id); //stop timer
-		client.end();
-	}
-}
+client.subscribe("telemetry",{qos:1});
 
-var options={
-	retain:true,
-	qos:1
-};
-var topic="testtopic";
-var message="test message";
-var topic_list=["topic2","topic3","topic4"];
-var topic_o={"topic22":0,"topic33":1,"topic44":1};
-console.log("subscribing to topics");
-client.subscribe("CALLBACKTOPIC",{qos:1}, function callback() {
-	console.log("CALLBACK FOR CALLBACKTOPIC");
-});
-//single topic
-client.subscribe(topic_list,{qos:1}); //topic list
-client.subscribe(topic_o); //object
-var timer_id=setInterval(function(){publish(topic,message,options);},5000);
-//notice this is printed even before we connect
-console.log("end of script");
 /**
  * Creates an HTTP server that acts as the entry point
  * to the REST services:
