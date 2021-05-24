4f36b0c89fc60b70448ced3464cb635ea3546ef8
Bulubuch <bulubuch@gmail.com>
2021-05-24 20:57:28 +0200

    Update device-dao

--- scripts/device.sql
+++ scripts/device.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS device (
-	id					INTEGER PRIMARY KEY NOT NULL,
-	uid					VARCHAR 16 NOT NULL,
-	model_name			VARCHAR 32 NOT NULL,
-	firmware_version	REAL NOT NULL,
-	name				VARCHAR 32,
-	when_created		INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE IF NOT EXISTS device(
+	id				INTEGER PRIMARY KEY NOT NULL,
+	name			TEXT UNIQUE NOT NULL,
+	description		TEXT,
+	manufacturer	TEXT,
+	datasheet		TEXT,
+	when_created	INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
