72817c6768f5d43b373834a554c277cdae0bb618
Bulubuch <bulubuch@gmail.com>
2021-05-26 15:52:58 +0200

    Update structure

--- admin/assets/script/cookies.js
+++ admin/assets/script/cookies.js
@@ -1,65 +1,62 @@
-class Cookies
+function getCookie(key) {
+	let c  = document.cookie;
+	let parts = c.split(";");
+	for (let i = 0; i < parts.length; i++)
+	{
+		let k = c.search(key + "=");
+		if (k >= 0) {
+			return c.slice(k + key.length + 1);
+		}
+	}
+	return (null);
+}
+function setCookie(key, value)
 {
-	get(key) {
-		let c  = document.cookie;
-		let parts = c.split(";");
-		for (let i = 0; i < parts.length; i++)
-		{
-			let k = c.search(key + "=");
-			if (k >= 0) {
-				return c.slice(k + key.length + 1);
-			}
-		}
-		return (null);
-	}
-	set(key, value)
+	let cookies = [];
+	let cookie = "";
+	let c  = document.cookie;
+	let k = -1;
+	let parts = c.split(";");
+	let i;
+	for (i = 0; i < parts.length; i++)
 	{
-		let cookies = [];
-		let cookie = "";
-		let c  = document.cookie;
-		let k = -1;
-		let parts = c.split(";");
-		let i;
-		for (i = 0; i < parts.length; i++)
-		{
-			k = c.search(key + "=");
-			if (k >= 0) {
-				continue;
-			} else {
-				cookies.push(c.split("="));
-			}
+		k = c.search(key + "=");
+		if (k >= 0) {
+			continue;
+		} else {
+			cookies.push(c.split("="));
 		}
-		cookies.push([key, value]);
-		for (i = 0; i < cookies.length; i++)
-		{
-			cookie += cookies[i][0] + "=" + cookies[i][1] + ";";
-		}
-		document.cookie = cookie;
 	}
-
-	del(key)
+	cookies.push([key, value]);
+	for (i = 0; i < cookies.length; i++)
 	{
-		let cookies = [];
-		let cookie = "";
-		let c  = document.cookie;
-		let k = -1;
-		let parts = c.split(";");
-		let i;
-		for (i = 0; i < parts.length; i++)
-		{
-			k = c.search(key + "=");
-			if (k >= 0) {
-				continue;
-			} else {
-				cookies.push(c.split("="));
-			}
-		}
-		for (i = 0; i < cookies.length; i++)
-		{
-			cookie += cookies[i][0] + "=" + cookies[i][1] + ";";
-		}
-		document.cookie = cookie;
+		cookie += cookies[i][0] + "=" + cookies[i][1] + ";";
 	}
+	document.cookie = cookie;
 }
 
-let cookies = Cookies();
+function delCookie(key)
+{
+	let cookies = [];
+	let cookie = "";
+	let c  = document.cookie;
+	let k = -1;
+	let parts = c.split(";");
+	let i;
+	for (i = 0; i < parts.length; i++)
+	{
+		k = c.search(key + "=");
+		if (k >= 0) {
+			continue;
+		} else {
+			cookies.push(c.split("="));
+		}
+	}
+	for (i = 0; i < cookies.length; i++)
+	{
+		cookie += cookies[i][0] + "=" + cookies[i][1] + ";";
+	}
+	document.cookie = cookie;
+}
+
+let cookies = new Cookies();
