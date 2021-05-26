969002c9a0ff4c64c87725bc4ba6b7781c76bf54
Bulubuch <bulubuch@gmail.com>
2021-05-26 14:09:19 +0200

    Update structure

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('It works!');
+	res.sendFile(path.join(__dirname, '../layout/index.html'));
 });
 
+router.get('/', (req, res) => {
+	res.send('It works!');
+  });
+  
 module.exports = router;
