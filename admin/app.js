00d86242ee4fa209adfd30ee41344e8adf0809fb
Bulubuch <bulubuch@gmail.com>
2021-05-26 14:13:14 +0200

    Update structure

--- admin/app.js
+++ admin/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const routes = require('./routes/index');
 
 const app = express();
+app.use(express.static('assets'))
 app.use('/', routes);
-
 module.exports = app;
