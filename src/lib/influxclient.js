f77d5b4b6606c843fe56a88274a50db7aa8f518f
Bulubuch <bulubuch@gmail.com>
2021-05-28 00:49:41 +0200

    Big changes

--- src/lib/influxclient.js
+++ src/lib/influxclient.js
@@ -1,11 +1,11 @@
 const Influx = require('influx')
 
 class influxClient {
-	static _initialized = null;
-	static _host
-	static _database
-	static _schema
-	static _db
+	_initialized = null;
+	_host
+	_database
+	_schema
+	_db
 
 	static init(host, database, schema) {
 		this._host = host;
